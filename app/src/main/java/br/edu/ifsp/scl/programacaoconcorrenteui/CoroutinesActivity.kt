8bd6fa699a952fa2f64db829470575f54e1306b8
Renato Luiz Carvalho <renato.carvalho@actminds.com>
2020-03-05 14:34:48 -0300

    Added Coroutine concepts

--- app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -1,12 +1,23 @@
 package br.edu.ifsp.scl.programacaoconcorrenteui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
+import kotlinx.coroutines.*
+import java.lang.Thread.sleep
 
-class CoroutinesActivity : AppCompatActivity() {
+class CoroutinesActivity : ProgramacaoConcorrenteActivity() {
+
+    val job = SupervisorJob();
+    val scope = CoroutineScope(Dispatchers.Default + job)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutines)
+    }
+
+    override fun run() {
+        scope.launch {
+            altaIv.setImageResource(R.drawable.android_preto)
+            baixaIv.setImageResource(R.drawable.android_verde)
+        }
     }
 }
